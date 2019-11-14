5cb0e6bc26c25d9fda8f9c012df9c62667e966c6
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-11-14 12:14:43 +0100

    Correct merging conflict

--- tools/python/live_viewer/mplplot.py
+++ tools/python/live_viewer/mplplot.py
@@ -36,37 +36,18 @@ class MplPlotCanvas(FigureCanvas):
         This method initialises the plot canvas object, creating an empty subplot
         within it.
         """
-<<<<<<< HEAD
-=======
         # Create a list of ADC part for plotting
         self.adc = ('Coarse', 'Fine', 'Gain')
 
->>>>>>> live_viewer
         # Create the figure canvas
         self.figure = Figure(dpi=dpi)
         FigureCanvas.__init__(self, self.figure)
         self.setParent(parent)
         FigureCanvas.setSizePolicy(self,
-<<<<<<< HEAD
-                                   QtWidgets.QSizePolicy.Expanding,
-                                   QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-        # Add the image axes to the figure
-        self.axes_gain = self.figure.add_subplot(131)
-        self.axes_coarse = self.figure.add_subplot(132)
-        self.axes_fine = self.figure.add_subplot(133)
-        self.axes_gain.set_xticks([])
-        self.axes_gain.set_yticks([])
-        self.axes_coarse.set_xticks([])
-        self.axes_coarse.set_yticks([])
-        self.axes_fine.set_xticks([])
-        self.axes_fine.set_yticks([])
-=======
                 QtWidgets.QSizePolicy.Expanding,
                 QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        
+
         # Add the image axes to the figure
         self.axes = (self.figure.add_subplot(1, 3, 1),
                    self.figure.add_subplot(1, 3, 2),
@@ -82,37 +63,18 @@ class MplPlotCanvas(FigureCanvas):
         self.axes[2].set_yticks([])
         self.axes[2].set_title(self.adc[2])
 
->>>>>>> live_viewer
 
         # Set up storage variables
         self.img_range = ()
         self.img_shape = None
-<<<<<<< HEAD
-        self.img_obj = None
-        self.colorbar = None
-        self.bar_orient = 'horizontal'
-
-        # Render an initial empty image frame
-        self.render_frame(np.zeros((10, 10), dtype=np.uint16),
-                          min_val=0,
-                          max_val=10)
-        self.figure.tight_layout()
-
-    def render_frame(self,
-                     img_data_gn,
-                     img_data_crs,
-                     img_data_fn,
-                     min_val=None,
-                     max_val=None):
-=======
         self.img_obj = {self.adc[0]: None,
-                        self.adc[1]: None, 
+                        self.adc[1]: None,
                         self.adc[2]: None}
         self.colorbar = {self.adc[0]: None,
                          self.adc[1]: None,
                          self.adc[2]: None}
         self.bar_orient = 'horizontal'
- 
+
         # Render an initial empty image frame
         tmp_img = (np.zeros((10, 10), dtype=np.uint16),
                    np.zeros((10, 10), dtype=np.uint16),
@@ -129,7 +91,6 @@ class MplPlotCanvas(FigureCanvas):
         self.render_frame(img_data[2], self.axes[2], self.adc[2])
 
     def render_frame(self, img_data, axe, adc, min_val=None, max_val=None):
->>>>>>> live_viewer
         """
         Render an image frame in the plot canvas.
 
@@ -137,11 +98,7 @@ class MplPlotCanvas(FigureCanvas):
         the image data is just updated if there is no change to the image shape, otherwise
         the axes and colorbar are redrawn.
         """
-<<<<<<< HEAD
         # If the minimum and/or maximum values are not defined, determine from the
-=======
-        # If the minimum and/or maximum values are not defined, determine from the 
->>>>>>> live_viewer
         # incoming image data
         if min_val is None:
             min_val = np.amin(img_data)
@@ -152,11 +109,7 @@ class MplPlotCanvas(FigureCanvas):
         # a redraw
         if self.img_shape != img_data.shape:
             self.img_shape = img_data.shape
-<<<<<<< HEAD
-            self.img_obj = None
-=======
             self.img_obj.update({adc: None})
->>>>>>> live_viewer
 
             # Set the colorbar orientation dependent on the aspect ratio of the image
             if self.img_shape[0] < self.img_shape[1]:
@@ -165,34 +118,6 @@ class MplPlotCanvas(FigureCanvas):
                 self.bar_orient = 'vertical'
 
             # Remove any existing colorbar prior to subequent re-draw
-<<<<<<< HEAD
-            if self.colorbar is not None:
-                self.colorbar.remove()
-                self.colorbar = None
-
-        # If no image object exists, draw one and add a colorbar
-        if self.img_obj is None:
-            self.img_obj = self.axes.imshow(img_data,
-                                            interpolation='nearest',
-                                            vmin=min_val,
-                                            vmax=max_val,
-                                            cmap='jet')
-
-            if self.colorbar is None:
-                cbar_ticks = np.linspace(min_val,
-                                         max_val,
-                                         self.cbar_numticks,
-                                         dtype=np.uint).tolist()
-                self.colorbar = self.figure.colorbar(self.img_obj,
-                                                     ax=self.axes,
-                                                     orientation=self.bar_orient,
-                                                     ticks=cbar_ticks)
-
-        # Otherwise just update the image data for speed
-        else:
-            self.img_obj.set_data(img_data)
-            self.axes.draw_artist(self.img_obj)
-=======
             if self.colorbar[adc] is not None:
                 self.colorbar[adc].remove()
                 self.colorbar.update({adc: None})
@@ -212,33 +137,18 @@ class MplPlotCanvas(FigureCanvas):
         else:
             self.img_obj[adc].set_data(img_data)
             axe.draw_artist(self.img_obj[adc])
->>>>>>> live_viewer
 
         # If the range of the data changed, update the colorbar accordingly
         if self.img_range != (min_val, max_val):
             self.img_range = (min_val, max_val)
-<<<<<<< HEAD
-            cbar_ticks = np.linspace(min_val,
-                                     max_val,
-                                     self.cbar_numticks,
-                                     dtype=np.uint).tolist()
-            self.colorbar.set_clim(min_val, max_val)
-            self.colorbar.set_ticks(cbar_ticks)
-            self.colorbar.draw_all()
-=======
             cbar_ticks = np.linspace(min_val, max_val, self.cbar_numticks, dtype=np.uint).tolist()
             self.colorbar[adc].set_clim(min_val, max_val)
             self.colorbar[adc].set_ticks(cbar_ticks)
             self.colorbar[adc].draw_all()
->>>>>>> live_viewer
 
         # Draw the frame
         self.draw()
 
-<<<<<<< HEAD
-
-=======
->>>>>>> live_viewer
 class MplNavigationToolbar(NavigationToolbar):
     """
     Navigation toolbar for the plot canvas.
